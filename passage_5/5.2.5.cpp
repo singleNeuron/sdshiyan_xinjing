c6a07db3afa777a896bb2b5928a5da1457ebfb2c
Neuron <liziyuan123456@yeah.net>
2016-12-21 16:53:46 +0800

    Add files via upload

--- passage_5/5.2.5.cpp
+++ passage_5/5.2.5.cpp
@@ -1,21 +1,21 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
-unsigned int m;
+unsigned int m,loca;
 int main () {
 	cin>>m;
-	int a[m+1],b[m][m+1],loca=m;
-	for (int i=1;i<=m;i++) cin>>a[m];
-	for (int i=1;i<=m-1;i++) {
+	unsigned int a[m+1][m],b[m+1];
+	for (int i=1;i<=m;i++) cin>>b[i];
+	for (int i=1;i<m;i++) {
+		loca=i+1;
 		for (int j=1;j<=m;j++) {
-			b[i][loca]=a[j];
+			a[j][i]=b[loca];
 			loca++;
-			if (loca>m) loca=1;
+			if (loca==m+1) loca=1;
 		}
-		loca=m+1-i;
 	}
-	for (int i=1;i<=m-1;i++) {
-		for (int j=1;j<=m;j++) cout<<b[i][j]<<" ";
+	for (int i=1;i<m;i++) {
+		for (int j=1;j<=m;j++) cout<<a[j][i]<<"\t";
 		cout<<endl;
 	}
 }
