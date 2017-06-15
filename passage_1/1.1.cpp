a57efbf9899667425662a9167f1eb7e1e04fae8d
Neuron <liziyuan123456@yeah.net>
2017-06-15 10:24:59 +0800

    Create 1.1.cpp

--- passage_1/1.1.cpp
+++ passage_1/1.1.cpp
@@ -1,8 +1,7 @@
 #include <cstdio>
 #include <iostream>
-using namespace std;
 int main () {
 	double a,b,c;
-	cin>>a>>b>>c;
-	cout<<(a+b+c)/3<<"\t"<<a*b*c;
+	std::cin>>a>>b>>c;
+	cout::(a+b+c)/3<<"\t"<<a*b*c;
 }
